332888aabae7e075cbe8c8ec62480290b2419319
Julien Atanassian <julien.atanassian@gmail.com>
2023-04-24 23:22:42 -0400

    implement strings sort

--- index.ts
+++ index.ts
@@ -1,7 +1,15 @@
 import { Sorter } from './sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
+// Sort array of numbers
 const numbers = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numbers);
-sorter.sort();
+const sorter1 = new Sorter(numbers);
+sorter1.sort();
+console.log(numbers.data);
+
+// Sort strings
+const characters = new CharactersCollection('test');
+const sorter2 = new Sorter(characters);
+sorter2.sort();
 console.log(numbers.data);
